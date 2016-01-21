3725bcca73598e889abbd795f0575cacf9caf61b
NAJThomas <nathalie.thomas@ft.com>
2016-01-21 09:58:51 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,8 +1,2 @@
-myname ('My name is Nathalie')
-print myname
-mypurpose ('I want to scrape BBC News for Davos stories')
-print mypurpose
-urltoscrape http://www.bbc.co.uk/news
-print urltoscrape
-newstories = 7
-print newstories
+myname Nathalie
+print my name
